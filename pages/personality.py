72b7362fec356b1b36afec383523369cd19b9c31
Suyashmkabra <suyashmkabra@gmail.com>
2025-03-25 02:40:17 +0530

    mpr sem7

--- pages/personality.py
+++ pages/personality.py
@@ -90,7 +90,6 @@ def main():
     end_idx = min((st.session_state.page_index + 1) * 7, 70)
 
     st.title("Personality Quiz")
-    st.image(r'assets\Virtual-AI-assistant.webp')
 
     # Generate questions for the current page
     selected_options = generate_questions(quesdata=questionData, startIndex=start_idx, endIndex=end_idx)
